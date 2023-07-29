7517ac35756c2cd9387cb21753e4c693e805a1dd
vermasonali1 <sv9210245@gmail.com>
2023-07-29 16:15:15 +0530

    adding Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,8 @@
+public class Multiply {
+    
+        public static void main(String[] args) {
+            int a=2,b=2;
+            System.out.println("The sum iss"+" "+ a*b);
+        
+    }
+}
