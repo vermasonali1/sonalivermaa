d14594dfd67065ee79bf6053a06e14feb1a45140
vermasonali1 <sv9210245@gmail.com>
2023-07-29 16:06:01 +0530

    adding sum.java,sum.class

--- sum.java
+++ sum.java
@@ -1,6 +1,6 @@
 public class sum{
     public static void main(String[] args) {
         int a=2,b=2;
-        System.out.println("The sum is"+a+b);
+        System.out.println("The sum iss"+" "+ a+b);
     }
 }
